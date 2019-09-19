6914f1c14641e5a4b250c14e323a489dfb515400
debuggerccai <cailiangwu@outlook.com>
2019-09-19 14:27:03 +0800

    feat: 提交代码

--- uni-scroll/src/components/uScroll/base/EventEmitter.js
+++ uni-scroll/src/components/uScroll/base/EventEmitter.js
@@ -1,111 +1,110 @@
-import {
-	warn
-} from '../utils'
+import { warn } from "../utils";
 
 export default class EventEmitter {
-	constructor(names = []) {
-		this.events = {}
-		this.eventTypes = {}
-		this.registerType(names)
-	}
+  constructor(names = []) {
+    this.events = {};
+    this.eventTypes = {};
+    this.registerType(names);
+  }
 
-	on(type, fn, context = this) {
-		this._checkInTypes(type)
-		if (!this.events[type]) {
-			this.events[type] = []
-		}
+  on(type, fn, context = this) {
+    this._checkInTypes(type);
+    if (!this.events[type]) {
+      this.events[type] = [];
+    }
 
-		// Warn: 直接引用参数 context 在小程序环境下会造成报错
-		this.events[type].push([fn, context])
-		return this
-	}
+	// Warn: 直接引用参数 context 在小程序环境下会造成报错
+    // this.events[type].push([fn, context]);
+    this.events[type].push([fn]);
+    return this;
+  }
 
-	once(type, fn, context = this) {
-		this._checkInTypes(type)
-		const magic = (...args) => {
-			this.off(type, magic)
+  once(type, fn, context = this) {
+    this._checkInTypes(type);
+    const magic = (...args) => {
+      this.off(type, magic);
 
-			fn.apply(context, args)
-		}
-		magic.fn = fn
+      fn.apply(context, args);
+    };
+    magic.fn = fn;
 
-		this.on(type, magic)
-		return this
-	}
+    this.on(type, magic);
+    return this;
+  }
 
-	off(type, fn) {
-		if (!type && !fn) {
-			this.events = {}
-			return this
-		}
+  off(type, fn) {
+    if (!type && !fn) {
+      this.events = {};
+      return this;
+    }
 
-		if (type) {
-			this._checkInTypes(type)
-			if (!fn) {
-				this.events[type] = []
-				return this
-			}
+    if (type) {
+      this._checkInTypes(type);
+      if (!fn) {
+        this.events[type] = [];
+        return this;
+      }
 
-			let events = this.events[type]
-			if (!events) {
-				return this
-			}
+      let events = this.events[type];
+      if (!events) {
+        return this;
+      }
 
-			let count = events.length
-			while (count--) {
-				if (
-					events[count][0] === fn ||
-					(events[count][0] && (events[count][0]).fn === fn)
-				) {
-					events.splice(count, 1)
-				}
-			}
+      let count = events.length;
+      while (count--) {
+        if (
+          events[count][0] === fn ||
+          (events[count][0] && events[count][0].fn === fn)
+        ) {
+          events.splice(count, 1);
+        }
+      }
 
-			return this
-		}
-	}
+      return this;
+    }
+  }
 
-	trigger(type, ...args) {
-		this._checkInTypes(type)
-		let events = this.events[type]
-		if (!events) {
-			return
-		}
+  trigger(type, ...args) {
+    this._checkInTypes(type);
+    let events = this.events[type];
+    if (!events) {
+      return;
+    }
 
-		let len = events.length
-		let eventsCopy = [...events]
-		let ret
-		for (let i = 0; i < len; i++) {
-			let event = eventsCopy[i]
-			let [fn, context] = event
-			if (fn) {
-				ret = fn.apply(context, args)
-				if (ret === true) break
-			}
-		}
-		return ret
-	}
+    let len = events.length;
+    let eventsCopy = [...events];
+    let ret;
+    for (let i = 0; i < len; i++) {
+      let event = eventsCopy[i];
+      let [fn, context] = event;
+      if (fn) {
+        ret = fn.apply(context, args);
+        if (ret === true) break;
+      }
+    }
+    return ret;
+  }
 
-	registerType(names) {
-		names.forEach((type) => {
-			this.eventTypes[type] = type
-		})
-	}
+  registerType(names) {
+    names.forEach(type => {
+      this.eventTypes[type] = type;
+    });
+  }
 
-	destory() {
-		this.events = {}
-		this.eventTypes = {}
-	}
+  destory() {
+    this.events = {};
+    this.eventTypes = {};
+  }
 
-	_checkInTypes(type) {
-		const types = this.eventTypes
-		const inTypes = types[type] === type
-		if (!inTypes) {
-			warn(
-				`EventEmitter has used unknown event type: "${type}", should be oneof [${Object.keys(
-				  types
-				)}]`
-			)
-		}
-	}
+  _checkInTypes(type) {
+    const types = this.eventTypes;
+    const inTypes = types[type] === type;
+    if (!inTypes) {
+      warn(
+        `EventEmitter has used unknown event type: "${type}", should be oneof [${Object.keys(
+          types
+        )}]`
+      );
+    }
+  }
 }
--- uni-scroll/src/pages/index/index.vue
+++ uni-scroll/src/pages/index/index.vue
@@ -1,58 +1,61 @@
 <template>
-	<view class="content">
-		<scroll>
-			<view class="line" v-for="(item, index) in dataList" :key="index">I am the No.{{ index }} line</view>
-		</scroll>
-	</view>
+  <view class="content">
+    <u-scroll>
+      <view class="line" v-for="(item, index) in dataList" :key="index">I am the No.{{ index }} line</view>
+    </u-scroll>
+  </view>
 </template>
 
 <script>
-	import Scroll from '@/components/uScroll/uScroll'
+import UScroll from "@/components/uScroll/uScroll";
 
-	export default {
-		data() {
-			return {
-				dataList: []
-			}
-		},
-		components: {
-			Scroll
-		},
-		methods: {
-			requestData() {
-				let arr = []
-				for (var i = 1; i <= 20; i++) {
-					arr.push(i)
-				}
-				this.dataList = this.dataList.concat(arr)
-			}
-		},
-		onLoad() {
-			this.requestData()
-		}
-	}
+export default {
+  data() {
+    return {
+      dataList: []
+    };
+  },
+  components: {
+    UScroll
+  },
+  methods: {
+    requestData() {
+      let arr = [];
+      for (var i = 1; i <= 20; i++) {
+        arr.push(i);
+      }
+      this.dataList = this.dataList.concat(arr);
+    }
+  },
+  onLoad() {
+    this.requestData();
+  }
+};
 </script>
 
 <style>
-	.content {
-		height: calc(100vh - 44px);
-	}
+.content {
+  height: 100vh;
+  /* #ifdef H5 */
+  height: calc(100vh - 44px);
+  /* #endif */
+}
 
-	.line {
-		height: 100upx;
-		line-height: 100upx;
-		padding-left: 40upx;
-		color: #606c71;
-		position: relative;
-	}
+.line {
+  height: 100upx;
+  line-height: 100upx;
+  padding-left: 40upx;
+  color: #606c71;
+  position: relative;
+}
 
-	.line::after {
-		content: "";
-		position: absolute;
-		left: -50%;
-		bottom: 0;
-		width: 200%;
-		border-bottom: 1px solid #e5e5e5;
-		transform: scale(0.5);
-	}
+.line::after {
+  content: "";
+  position: absolute;
+  left: -50%;
+  bottom: 0;
+  width: 200%;
+  border-bottom: 1px solid #e5e5e5;
+  transform: scale(0.5);
+}
 </style>
